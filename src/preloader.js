0a14f29879c14f5e55ab183d463f5d2e18585b3f
Victor Magalhães <victorhfmag@gmail.com>
2019-03-23 10:10:33 -0300

    Preload images

---
+++ src/preloader.js
@@ -0,0 +1,18 @@
+const images = [
+  "assets/images/websightdemo.gif",
+  "assets/images/autocad-web.png",
+  "assets/images/macos-performance-unity.png",
+  "assets/images/codesize-unity.png",
+  "assets/images/sourcemap-decoder-benchmark.png",
+  "assets/images/cloudflare-isolates.png",
+  "assets/images/cloudflare-faas-benchmark.png",
+  "assets/images/wasmer-nginx.png"
+];
+
+setTimeout(() => {
+  images.forEach(url => {
+    const img = new Image();
+    img.src = url;
+    console.log(img);
+  });
+}, 0);
